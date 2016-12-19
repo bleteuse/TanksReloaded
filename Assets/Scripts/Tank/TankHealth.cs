936bbca0be8bf66cd63a0b20d5201f66b41557af
Louis Grasset <olgret38@gmail.com>
2016-12-19 10:05:03 +0100

    First commit, original project

---
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+    public float m_StartingHealth = 100f;   //energie de début       
+    public Slider m_Slider;     //référence au Slider                   
+    public Image m_FillImage;  // Nous cherchons à modifier la couleur de l'image qui doit être renseignée dans cet attribut                    
+	public Color m_FullHealthColor = Color.green; //Couleur lorsque l'énergie est égale à 100   
+    public Color m_ZeroHealthColor = Color.red;  //Couleur lorsque l'énergie est égale à 0   
+    public GameObject m_ExplosionPrefab; //référence au Prefab d'explosion
+    
+    
+    private AudioSource m_ExplosionAudio; //reference à l'audio source du TankExplosion          
+    private ParticleSystem m_ExplosionParticles; //reference au système de particule   
+    private float m_CurrentHealth; //reference à l'état d'énergie actuel 
+    private bool m_Dead;   // Tank est mort ou pas ?         
+
+
+    private void Awake()
+    {
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>(); //Instantier le systeme de particule du prefab
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>(); //réference au composant audio du système de particules.
+
+        m_ExplosionParticles.gameObject.SetActive(false); //Désactiver le GameObject pour le moment
+    }
+
+
+    private void OnEnable() //Lorsque le Tank est activé
+    {
+        m_CurrentHealth = m_StartingHealth; //Initialiser le niveau d'énergie à 100
+        m_Dead = false; //Le Tank n'est pas mort
+
+        SetHealthUI(); //Ajuster la valeur et la couleur du Slider
+    }
+    
+
+    public void TakeDamage(float amount) //L'argument amount peut être variable. Par exemple si l'obus est trop prêt alors le dégat est plus conséquent que
+	//si l'obus est plus loin.
+    {
+        // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+		m_CurrentHealth-= amount; //On met à jour l'énergie du Tank mais on est pas entrain de déssiner
+		SetHealthUI (); //On met à jour le dessin du Slider
+		if (m_CurrentHealth <= 0f && !m_Dead) { //Si l'énergie est inférieure ou égale à 0 et on n'est pas déjà mort
+			OnDeath ();
+		}
+    }
+
+
+    private void SetHealthUI()
+    {
+        // Adjust the value and colour of the slider.
+		m_Slider.value = m_CurrentHealth;
+		m_FillImage.color = Color.Lerp (m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth); //le troisième argument est une sorte
+		//de pourcentage qui reflète le pourcentage de niveau d'enérgie qu'on possède.
+    }
+
+
+    private void OnDeath()
+    {
+        // Play the effects for the death of the tank and deactivate it.
+		m_Dead = true;
+		m_ExplosionParticles.transform.position = transform.position; //Placer le GameObject des particles à la position du Tank
+		m_ExplosionParticles.gameObject.SetActive (true);//Activer le gameObject parce qu'il a été désactivé au début
+
+		m_ExplosionParticles.Play (); //jouer le mouvement des particules
+		m_ExplosionAudio.Play (); //jouer le son de l'explosion
+
+		gameObject.SetActive(false); //désactiver le Tank.
+    }
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;   //Nous devons savoir quel joueur on va manipuler pour activer ses boutons correspondants. Par exemple Fire1 pour Player1...     
+    public Rigidbody m_Shell; // reférence à l'obus qu'on va instancier.          
+    public Transform m_FireTransform; // réference à l'endroit à partir duquel on va tirer l'obus   
+    public Slider m_AimSlider; //référence au Slider pour l'étirer et le rétrécir           
+    public AudioSource m_ShootingAudio; // réference à l'audio qui va être joué lors du tir  
+    public AudioClip m_ChargingClip; //un audio clip pour charger     
+    public AudioClip m_FireClip;// un audio clip pour tirer         
+    public float m_MinLaunchForce = 15f; //Correspondent aux valeurs du Min Max du Slider
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f; //La temps maximal de chargement
+
+    
+    private string m_FireButton; // Le bouton de tir qui va être FireX. X=1,2,3... le numéro du joueur.         
+    private float m_CurrentLaunchForce; //La valeur actuelle du chargement  
+    private float m_ChargeSpeed; //la vitesse de transition entre min et max         
+    private bool m_Fired; // un bouléen qui renseigne si l'obus a été tiré ou pas              
+
+
+    private void OnEnable() //initialisation des valeurs
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce; 
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber; //Calculer la valeur de Fire1 pour joueur1, Fire2 pour joueur2 ....
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime; //Calculer la vitesse = distance/temps
+    }
+    
+
+    private void Update()
+    {
+        // Track the current state of the fire button and make decisions based on the current launch force.
+		m_AimSlider.value = m_MinLaunchForce; //on remet la valeur du slider à son minimum
+		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) { //nous avons chargé mais pas encore tiré.
+			m_CurrentLaunchForce = m_MaxLaunchForce; //On bloque la valeur maximale du tir sur la valeur max prédeterminée. 
+			Fire (); //On tire
+		} else if (Input.GetButtonDown (m_FireButton)) { //Vérifier si on a cliqué sur le bouton de tir pour la première fois
+			m_Fired = false; //on n'a pas encore tiré
+			m_CurrentLaunchForce = m_MinLaunchForce; //on commence par la valeur min
+			m_ShootingAudio.clip = m_ChargingClip; //jouer le son du tir
+			m_ShootingAudio.Play ();
+
+		} else if (Input.GetButton (m_FireButton) && !m_Fired) { //Vérifier si le bouton est enfoncé mais on n'a pas encore tiré
+			m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime; //incrémenter la force du tir mais on n'a pas encore mis à jour le Slider
+			m_AimSlider.value = m_CurrentLaunchForce; //Mettre à jour la valeur du Slider.
+
+		} else if (Input.GetButtonUp (m_FireButton) && !m_Fired) { //vérifier si on a relaché le bouton de tir mais on n'a pas encore tiré
+			Fire(); //On tire 
+		}
+    }
+
+
+    private void Fire() //On va instancier l'obus et le lancer suivant la valeur de tir qu'on determinée
+    {
+        // Instantiate and launch the shell.
+		m_Fired = true ; //on a tiré
+		Rigidbody shellInsatnce = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;//instancier l'obus à la position de l'objet de
+		//tir ainsi qu'à sa rotation. L'objet concerné est le FireTransform. Le résultat de la fonction Instantiate est un objet on le convertit en Rigibody.
+		shellInsatnce.velocity = m_CurrentLaunchForce * m_FireTransform.forward; //La vitesse de l'obus est égale à la force du tir multipliée par le vecteur
+		//avant du GameObject FireTransform.
+
+		m_ShootingAudio.clip = m_FireClip; //jouer le son du tir
+		m_ShootingAudio.Play ();
+
+		m_CurrentLaunchForce = m_MinLaunchForce; //remettre à la valeur initiale la valeur de chargement
+    }
+}
